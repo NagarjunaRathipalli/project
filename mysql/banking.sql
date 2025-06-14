5d47b95755df6ecd63dfeb52acc6113101723579
psyashaswi <psyashaswi9@gmail.com>
2025-06-14 13:12:49 +0530

    added table

--- mysql/banking.sql
+++ mysql/banking.sql
@@ -1,11 +1,19 @@
-CREATE TABLE customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    date_of_birth DATE,
-    gender ENUM('Male', 'Female', 'Other'),
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
     email VARCHAR(100),
-    phone VARCHAR(15),
-    address TEXT,
+    phone VARCHAR(10),
+    password VARCHAR(255), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+CREATE TABLE Accounts (
+    user_id int,
+    account_id INT PRIMARY KEY AUTO_INCREMENT,
+    account_type ENUM('Savings', 'Checking', 'Loan'),
+    balance DECIMAL(15,2) DEFAULT 0.00,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
