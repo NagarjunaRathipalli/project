6b4fb6e8546641cc6937a3a362341c604447b4ef
psyashaswi <psyashaswi9@gmail.com>
2025-06-16 13:04:45 +0530

    added2tables

--- mysql/banking.sql
+++ mysql/banking.sql
@@ -17,3 +17,19 @@ CREATE TABLE Accounts (
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
+CREATE TABLE Transactions (
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT,
+    transaction_type ENUM('Deposit', 'Withdrawal', 'Transfer'),
+    amount DECIMAL(15,2),
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description TEXT,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
+);
+
+
+CREATE TABLE Admins (
+    admin_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50),
+    password VARCHAR(100)
+);
